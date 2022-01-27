32787cfa00a1b0de30d04b2b13f5034bacecebef
mErlin-sp <merlin.ua.1@gmail.com>
2022-01-27 08:33:19 +0200

    Added functionality for Task 1

--- public/js/script.js
+++ public/js/script.js
@@ -1,2 +1,130 @@
+let start_button, stop_button, reload_button, anim, square
+//
+let started = false
+//
+let angle = 90
+let dist = 20
+let speed = 500
+
 $(document).ready(function () {
+    let work = $('#work')
+    anim = $('#anim')
+    start_button = $('#start_button')
+    stop_button = $('#stop_button').hide()
+    reload_button = $('#reload_button').hide()
+    square = $('#square')
+
+    move_square_to_start()
+    animate()
+
+    work.hide()
+
+    $('#play_button').click(function () {
+        localStorage.setItem('animation_log', JSON.stringify([]));
+        message_handler('Animation window opened.')
+        work.fadeIn()
+    })
+
+    $('#close_button').click(function () {
+        if (started) {
+            stop_button.click()
+        }
+
+        message_handler('Animation window closed.')
+        work.fadeOut()
+
+        let animation_log = $('#animation_log')
+        animation_log.innerHTML = ''
+
+        JSON.parse(localStorage.getItem('animation_log')).forEach(function (message) {
+            animation_log.append(`<p>${message}</p>`)
+        })
+    })
+
+    start_button.click(function () {
+        message_handler(`Animation started. Angle: ${round_two(angle)}.`)
+        start_button.hide()
+        stop_button.show()
+        started = true
+    })
+
+    stop_button.click(function () {
+        message_handler('Animation stopped.')
+        stop_button.hide()
+        start_button.show()
+        started = false
+    })
+
+    reload_button.click(function () {
+        message_handler('Animation reloaded.')
+        reload_button.hide()
+        start_button.show()
+        started = false
+        move_square_to_start()
+    })
 });
+
+function move_square_to_start() {
+    angle = Math.random() * 180
+    let start_x = Math.random() * (anim[0].clientWidth - parseFloat(square.css('width')))
+    console.log(angle + '  ' + anim[0].clientWidth)
+    square.animate({'left': start_x + 'px', 'top': 0}, 0).show()
+
+}
+
+function animate() {
+    let id = setInterval(frame, speed);
+
+    function frame() {
+        let fade_out = false
+        if (!started) {
+            return
+        }
+
+        let max_x = anim[0].clientWidth - parseFloat(square.css('width'))
+        let max_y = anim[0].clientHeight - parseFloat(square.css('height'))
+
+        let x = (Math.cos(angle * Math.PI / 180) * dist) + parseFloat(square.css('left'));
+        let y = (Math.sin(angle * Math.PI / 180) * dist) + parseFloat(square.css('top'));
+
+        if (x <= 0) {
+            angle -= 90
+            message_handler(`Left vertical border reached. New angle: ${round_two(angle)}.`)
+            x = 0
+        } else if (x >= max_x) {
+            angle += 90
+            message_handler(`Right vertical border reached. New angle: ${round_two(angle)}.`)
+            x = max_x
+        }
+
+        if (y >= max_y) {
+            message_handler('Animation ended')
+            fade_out = true
+            started = false
+
+            stop_button.hide()
+            reload_button.show()
+        }
+
+        square.animate({left: `${x}px`, top: `${y}px`});
+        if (fade_out) {
+            square.fadeOut(1500)
+        }
+        console.log(x + '  ' + y)
+    }
+}
+
+function message_handler(message) {
+    message = new Date().toLocaleTimeString() + '  ' + message
+    console.log(message)
+    $('#message').html(message)
+
+    let log = JSON.parse(localStorage.getItem('animation_log'))
+    log.push(message)
+    localStorage.setItem('animation_log', JSON.stringify(log))
+}
+
+function round_two(num) {
+    return Math.round(num * 100) / 100
+}
+
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', function (req, res) {
-    res.send('Admin page.');
+const lorem_ipsum = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eleifend, orci in vehicula iaculis, nisl\n' +
+    '        ligula convallis massa, at venenatis lorem tortor at libero. Vivamus suscipit auctor felis, ac bibendum\n' +
+    '        magna venenatis tempor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos\n' +
+    '        himenaeos. Nulla tempor lacinia ipsum ut faucibus. Suspendisse placerat et mi eu euismod. Donec non\n' +
+    '        scelerisque enim. Donec dictum ullamcorper est non egestas. Aliquam eu quam vel ex lobortis egestas at non\n' +
+    '        est. Sed mi nibh, eleifend a tempus id, mattis at turpis. Fusce mi erat, aliquet quis urna ac, tempor\n' +
+    '        fermentum odio. Sed vel vulputate dolor. Morbi dolor lorem, aliquam at accumsan et, varius ac turpis. Aenean\n' +
+    '        tristique id augue sed tempor. Maecenas vitae semper est.'
+
+
+/* GET admin page. */
+router.get('/', function (req, res, next) {
+    const data = {
+        title: 'Admin page',
+        lorem_ipsum: lorem_ipsum
+    }
+    res.render('admin', data);
 });
 
 module.exports = router;
